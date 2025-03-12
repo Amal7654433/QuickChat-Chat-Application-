2cdd367e3a7d70ca551302ac67b4a53d5c76443f
Amal7654433 <121153174+Amal7654433@users.noreply.github.com>
2025-03-12 17:07:28 +0530

    chnges

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,14 @@ import authRoute from './routes/auth.route.js'
 import messageRoute from './routes/message.route.js'
 import cookieParser from 'cookie-parser'
 import errorHandler from './middlewares/error.middleware.js';
-import {app,server} from './config/socketio.js'
+import { app, server } from './config/socketio.js'
 
 import path from "path";
 const __dirname = path.resolve();
 dotenv.config()
-app.use(express.json({ limit: "10mb" })); 
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-
+const PORT = process.env.PORT
 // app.use(express.json())
 // app.use(express.urlencoded({ extended: true }));
 app.use(cors({
@@ -30,12 +30,12 @@ app.use(messageRoute)
 app.use(errorHandler);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
+
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
-  }
-server.listen(5001, () => {
+}
+server.listen(PORT, () => {
     connectDb()
     console.log('servr running')
 })
